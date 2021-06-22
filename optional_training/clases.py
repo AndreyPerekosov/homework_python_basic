e2d67415d7bdb1daee35cba2ed7e16a9b129cad4
Andrey Perekosov <perekosov@gmail.com>
2021-06-22 15:34:25 +0300

    done optional task clases

--- optional_training/clases.py
+++ optional_training/clases.py
@@ -8,6 +8,7 @@ class User():
         self._pets = []
         for pet in pets:
             self.pets.append(pet)
+            self.init_user(pet)
 
     @property
     def password(self):
@@ -27,11 +28,25 @@ class User():
 
     def __iadd__(self, item):
         self._pets.append(item)
+        self.init_user(item)
         return self
 
-def t(*pets):
-    data = []
-    for pet in pets:
-        data.append(pet)
-    return data
-print(t())
+    def init_user(self, pet):
+        pet.user = self
+
+
+class Pet():
+    def __init__(self, name, kind, birth_date):
+        self.name = name
+        self.kind = kind
+        self.birth_date = birth_date
+        self.user = None
+
+
+p_1 = Pet('tommy', 'cat', '03.06.18')
+p_2 = Pet('ho', 'dog', '03.06.18')
+u_1 = User('John', 'Smith', 'j@mail.com', '123', 'qwert', p_1, p_2)
+p_3 = Pet('jacky', 'cat', '03.06.18')
+p_4 = Pet('bo', 'dog', '03.06.18')
+u_2 = User('Tom', 'Sawyer', 't@mail.com', '123', 'qwert', p_3)
+u_2.pets = p_4
