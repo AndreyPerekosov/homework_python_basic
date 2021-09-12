15fc18f215f450675218cc238ef0d473d61d5ef0
Andrey Perekosov <perekosov@gmail.com>
2021-09-13 00:15:52 +0300

    done logic

--- homework_06/views/users.py
+++ homework_06/views/users.py
@@ -1,8 +1,12 @@
+import logging
 from flask import Blueprint, request, render_template, redirect, url_for
-from werkzeug.exceptions import NotFound
+from sqlalchemy.exc import IntegrityError, DatabaseError
+from werkzeug.exceptions import NotFound, BadRequest, InternalServerError
 
-from models import User
+from models import User, Post
 from models.database import db
+
+log = logging.getLogger(__name__)
 
 users_app = Blueprint("users_app", __name__)
 
@@ -18,5 +22,29 @@ def get_user(user_id: int):
     user = User.query.filter_by(id=user_id).one_or_none()
     if user is None:
         raise NotFound(f"No user for id {user_id}")
-    posts = user.posts
-    return render_template("users/detail.html", user=user, posts=posts)
+    return render_template("users/detail.html", user=user)
+
+
+@users_app.route("/<int:user_id>/add", methods=["GET", "POST"], endpoint="add")
+def create_post(user_id: int):
+    user = User.query.filter_by(id=user_id).one_or_none()
+    if user is None:
+        raise NotFound(f"No user for id {user_id}")
+    if request.method == "GET":
+        return render_template("users/add.html", user=user)
+    post_title = request.form.get("post-title")
+    post_body = request.form.get("post-body")
+    if not post_title or not post_body:
+        raise BadRequest("Please fill in title and body")
+    post = Post(title=post_title, body=post_body)
+    user.posts.append(post)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        log.exception("Could not add the post, got integrity error")
+        db.session.rollback()
+    except DatabaseError:
+        log.exception("Could not add the post, got database error")
+        db.session.rollback()
+        raise InternalServerError("Error adding the post")
+    return redirect(url_for("users_app.detail", user_id=user.id))
